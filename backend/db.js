b560dd036963699e6df2c3089291b9a5384fd773
Iamirtizaali <aliirtiza859@gmail.com>
2024-12-08 12:12:03 +0500

    connection string added

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config({ path: ".env" });
 //mongoose.connect('mongodb+srv://aliirtiza859:Irtizaali859.@irtizacluster.l7kp5.mongodb.net/');
-mongoose.connect("mongodb://localhost:27017/GiftShare")
+mongoose.connect(process.env.MONGO_URL)
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to MongoDB'));
