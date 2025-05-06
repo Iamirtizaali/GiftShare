5938372d1cd0040146ac4b2968920d7386698c95
Irtiza Ali <142350791+Iamirtizaali@users.noreply.github.com>
2025-05-06 19:10:37 +0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 require("dotenv").config({ path: ".env" });
-//mongoose.connect('mongodb+srv://aliirtiza859:Irtizaali859.@irtizacluster.l7kp5.mongodb.net/');
 mongoose.connect(process.env.MONGO_URL)
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
